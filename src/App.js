4aa6deb097f3cae08db688734914d907c4752bdb
SalamatovAhmed <_email@example.com>
2026-04-22 22:43:47 +0600

    rebuild for vercel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { ThemeProvider } from './context/ThemeContext';
